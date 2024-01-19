6decfa24b709a0974820f0567a0651d4c280209e
Jay Guo <jguo144@gmail.com>
2024-01-19 15:12:43 -0500

    Fix routing for metadata request (#6)

--- ckanext/odata/views.py
+++ ckanext/odata/views.py
@@ -5,15 +5,15 @@ from ckanext.odata import utils
 odata_blueprint = Blueprint(u'odata_blueprint', __name__)
 
 
-def odata(uri):
-    return utils.odata(uri)
-
 def odata_metadata():
     return utils.odata_metadata()
 
+def odata(uri):
+    return utils.odata(uri)
 
-odata_blueprint.add_url_rule('/datastore/odata3.0/<uri>', view_func=odata)
+
 odata_blueprint.add_url_rule('/datastore/odata3.0/$metadata', view_func=odata_metadata)
+odata_blueprint.add_url_rule('/datastore/odata3.0/<uri>', view_func=odata)
 
 
 def get_blueprints():
